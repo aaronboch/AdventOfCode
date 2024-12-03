faa8a22cbc7047fc965cb378d47b10aa20791475
aaronboch <aaronboch8@gmail.com>
2024-12-03 21:42:17 +0100

    D2

---
+++ 2024/Day2/p2.cpp
@@ -0,0 +1,53 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+int main() {
+  int result = 0;
+  std::ifstream input("../Input/02");
+  std::string inputLine = "1 3 2 4 5";
+
+  while (getline(input, inputLine)) {
+    std::vector<int> line;
+    std::istringstream lineStream{inputLine};
+    int test;
+    while (lineStream >> test) {
+      line.push_back(test);
+    }
+    if (!line.empty()) {
+      int previous = line.at(0);
+      bool safe = true;
+      bool first = true;
+      bool desc = true;
+      bool damp = true;
+
+      for (int i = 1; i < (int)line.size(); i++) {
+        int cur = line.at(i);
+        int diff = previous - cur;
+        if ((first || desc) && diff <= 3 && diff > 0) {
+          if (first) {
+            first = false;
+          }
+        } else if ((first || !desc) && diff >= -3 && diff < 0) {
+          if (first) {
+            first = false;
+            desc = false;
+          }
+        } else if (damp) {
+          damp = false;
+        } else {
+          safe = false;
+          break;
+        }
+        previous = cur;
+      }
+      if (safe) {
+        result++;
+      }
+    } else {
+      std::cerr << "Error: Parsed line is empty!" << std::endl;
+    }
+  }
+  std::cout << result << std::endl;
+}
