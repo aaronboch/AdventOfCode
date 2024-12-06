fc46e45a09c089fb1fdd7282f32a6f0c5572652b
aaronboch <aaronboch8@gmail.com>
2024-12-06 16:38:54 +0100

    D6p2 very bad solution

--- 2024/Day6/p2.cpp
+++ 2024/Day6/p2.cpp
@@ -2,12 +2,12 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
-void moveGuard(std::vector<std::string> &lines, int &y);
 int main() {
-  std::ifstream file{"../Input/06t"};
+  std::ifstream file{"../Input/06"};
   std::string iLine;
   std::vector<std::string> lines;
   std::vector<std::string> linesCP;
+  int result = 0;
   while (getline(file, iLine)) {
     lines.push_back(iLine);
   }
@@ -17,86 +17,119 @@ int main() {
   }
   for (int j = 0; j < (int)lines.size(); j++) {
     for (int i = 0; i < (int)lines[j].size(); i++) {
+      std::cout << "j: " << j << " i: " << i << std::endl;
+
       if (lines[j][i] == '#' || lines[j][i] == '^') {
         continue;
       }
       lines[j][i] = 'O';
-      for (int y = 0; y < (int)lines.size(); y++) {
-        moveGuard(lines, y);
-        // anderer ansatzt fuers loopen = wenn er mehr als die anzahl von
-        // stellen der matrix beruehrt hat
-      }
-      std::cout << std::endl;
-      int result = 0;
-      for (auto l : lines) {
-        std::cout << l << std::endl;
-      }
-      lines = linesCP;
-      std::cout << "j: " << j << " i: " << i << std::endl;
-    }
-  }
-  return 0;
-}
+      /*
+        for (auto l : lines) {
+          std::cout << l << std::endl;
+        }
+      */
 
-void moveGuard(std::vector<std::string> &lines, int &y) {
-  int x = lines[y].find_first_of("^><j");
-  if (x > 0) {
-    if (lines[y][x] == '^') {
-      if (y - 1 >= 0) {
-        if (lines[y - 1][x] == '#') {
-          lines[y][x] = '>';
-          y = 0;
-        } else if (lines[y - 1][x] == 'O') {
-          lines[y][x] = '>';
-          y = 0;
-        } else {
-          lines[y - 1][x] = '^';
-          lines[y][x] = 'X';
-          y = 0;
+      int steps = 0;
+      for (int y = 0; y < (int)lines.size(); y++) {
+        int x = lines[y].find_first_of("^><j");
+
+        if (x > 0 && steps < (int)lines.size() * (int)lines[0].size()) {
+          // std::cout << lines[y][x] << " at y: " << y << "with: " << steps <<
+          // " steps" << std::endl;
+          if (lines[y][x] == '^') {
+            if (y - 1 >= 0) {
+              if (lines[y - 1][x] == '#') {
+                lines[y][x] = '>';
+                y = 0;
+              } else if (lines[y - 1][x] == 'O') {
+                lines[y][x] = '>';
+                y = 0;
+              } else {
+                lines[y - 1][x] = '^';
+                lines[y][x] = '.';
+                y = 0;
+                steps++;
+              }
+            } else {
+              steps = 0;
+              break;
+            }
+
+          } else if (lines[y][x] == '>') {
+            if (x + 1 < (int)lines[y].size()) {
+              if (lines[y][x + 1] == '#') {
+                lines[y][x] = 'j';
+                y = 0;
+              } else if (lines[y][x + 1] == 'O') {
+                lines[y][x] = 'j';
+                y = 0;
+              } else {
+                lines[y][x + 1] = '>';
+                lines[y][x] = '.';
+
+                y = 0;
+                steps++;
+              }
+            } else {
+              steps = 0;
+              break;
+            }
+          } else if (lines[y][x] == 'j') {
+            if (y + 1 < (int)lines.size()) {
+              if (lines[y + 1][x] == '#') {
+                lines[y][x] = '<';
+                y = 0;
+              } else if (lines[y + 1][x] == 'O') {
+                lines[y][x] = '<';
+                y = 0;
+              } else {
+                lines[y + 1][x] = 'j';
+                lines[y][x] = '.';
+
+                y = 0;
+                steps++;
+              }
+            } else {
+              steps = 0;
+              break;
+            }
+          } else if (lines[y][x] == '<') {
+            if (x - 1 >= 0) {
+              if (lines[y][x - 1] == '#') {
+                lines[y][x] = '^';
+                y = 0;
+              } else if (lines[y][x - 1] == 'O') {
+                lines[y][x] = '^';
+                y = 0;
+              } else {
+                lines[y][x - 1] = '<';
+                lines[y][x] = '.';
+                y = 0;
+                steps++;
+              }
+            } else {
+              steps = 0;
+              break;
+            }
+          }
+        } else if (steps >= (int)lines.size() * (int)lines[0].size()) {
+          // std::cout << steps << ">=" << (int)lines.size() *
+          // (int)lines[0].size()
+          //          << " i: " << i << std::endl;
+          result++;
+          break;
         }
       }
-    } else if (lines[y][x] == '>') {
-      if (x + 1 < (int)lines[y].size()) {
-        if (lines[y][x + 1] == '#') {
-          lines[y][x] = 'j';
-          y = 0;
-        } else if (lines[y][x + 1] == 'O') {
-          lines[y][x] = 'j';
-          y = 0;
-        } else {
-          lines[y][x + 1] = '>';
-          lines[y][x] = 'X';
-          y = 0;
+      /*if (j == 6 && i == 3) {
+        std::cout << std::endl;
+
+        for (auto l : lines) {
+          std::cout << l << std::endl;
         }
-      }
-    } else if (lines[y][x] == 'j') {
-      if (y + 1 < (int)lines.size()) {
-        if (lines[y + 1][x] == '#') {
-          lines[y][x] = '<';
-          y = 0;
-        } else if (lines[y + 1][x] == 'O') {
-          lines[y][x] = '<';
-          y = 0;
-        } else {
-          lines[y + 1][x] = 'j';
-          lines[y][x] = 'X';
-          y = 0;
-        }
-      }
-    } else if (lines[y][x] == '<') {
-      if (x - 1 >= 0) {
-        if (lines[y][x - 1] == '#') {
-          lines[y][x] = '^';
-          y = 0;
-        } else if (lines[y][x - 1] == '0') {
-          lines[y][x] = '^';
-          y = 0;
-        } else {
-          lines[y][x - 1] = '<';
-          lines[y][x] = 'X';
-          y = 0;
-        }
-      }
+      }*/
+      lines = linesCP;
     }
   }
+  std::cout << result << std::endl;
+  return 0;
 }
